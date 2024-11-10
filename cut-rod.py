d7ff0de02e938558dadff1ae67f7d57580666df7
James Clarke <james.clarke@tcu.edu>
2024-11-10 16:57:24 -0600

    complete

--- cut-rod.py
+++ cut-rod.py
@@ -2,20 +2,31 @@ import sys
 from typing import List
 
 def memoized_cut_rod(p: List[int], n: int) -> int:
+    # initialize array of -inf
+    r: List[int] = [-sys.maxsize - 1] * (n + 1)
+    return memoized_cut_rod_aux(p, n, r)
 
-    # initialize empty array
-    r: List[int] = []
+def memoized_cut_rod_aux(p: List[int], n: int, r: List[int]) -> int:
+    # base case check
+    if r[n] >= 0:
+        return r[n]
+    elif n == 0:
+        q = 0
 
-    # assign array vals to +inf
-    for i in range(n):
-        r[i] = sys.maxsize
-
+    else:
+        q = -sys.maxsize - 1
+        for i in range(1, n + 1):
+            q = max(q, p[i - 1] + memoized_cut_rod_aux(p, n - i, r))
     
-    
-    return 0
+    r[n] = q
+    return q
+
 
 def main():
-    memoized_cut_rod([1,2,3,4,5], 5)
+    p = [1, 2, 3, 4, 5]
+    n = 5
+    print(f"Max revenue: {memoized_cut_rod(p, n)}")
 
 if __name__ == "__main__":
     main()
+
